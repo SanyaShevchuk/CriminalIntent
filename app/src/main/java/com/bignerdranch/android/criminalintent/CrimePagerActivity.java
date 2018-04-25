9b0eb82936ebeaa65833fdbeeb058bf9e2d326a4
Sanya Shevchuk <shevchuksanya983@gmail.com>
2018-04-25 09:49:46 +0200

    Regeneration field CrimeFragment and copletely correct add 'to first' and 'to last' buttons with addOnPageChangeListener

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimePagerActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimePagerActivity.java
@@ -3,7 +3,10 @@ package com.bignerdranch.android.criminalintent;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
@@ -11,6 +14,9 @@ import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.Button;
+import android.widget.Toast;
 
 import java.util.List;
 import java.util.UUID;
@@ -25,16 +31,45 @@ public class CrimePagerActivity extends AppCompatActivity {
             "com.bignerdranch.android.criminalintent.crime_id";
     private ViewPager mViewPager;
     List<Crime> mCrimes;
+    private Button mFirstButton;
+    private Button mLastButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime_pager);
+        mFirstButton = (Button) findViewById(R.id.first_button);
+        mLastButton = (Button) findViewById(R.id.last_button);
         UUID crimeId = (UUID) getIntent().getSerializableExtra(EXTRA_CRIME_ID);
         mViewPager = (ViewPager) findViewById(R.id.crime_pager);
+        mViewPager.setOffscreenPageLimit(1);
         mCrimes = CrimeLab.get(this).getCrimes();
 
+
         FragmentManager fragmentManager = getSupportFragmentManager();
+
+        /**
+         * In old version you can use ViewPager.setOnPageChangeListener, but now it
+         * is depricated, and addOnPageChangeListener is alternative change
+         */
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if(position==mCrimes.size()-1) mLastButton.setEnabled(false);
+                if(position==0) mFirstButton.setEnabled(false);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
         mViewPager.setAdapter(new FragmentStatePagerAdapter(fragmentManager) {
             @Override
             public int getCount() {
@@ -43,7 +78,11 @@ public class CrimePagerActivity extends AppCompatActivity {
 
             @Override
             public Fragment getItem(int position) {
+                if(mViewPager.getCurrentItem()==0) updateButton(false,true);
+                else if(mViewPager.getCurrentItem()==mCrimes.size()-1) updateButton(true,false);
+                else updateButton(true, true);
                 Crime crime = mCrimes.get(position);
+
                 return CrimeFragment.newIntent(crime.getId());
             }
         });
@@ -54,6 +93,33 @@ public class CrimePagerActivity extends AppCompatActivity {
                 break;
             }
         }
+
+        mFirstButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mViewPager.getCurrentItem()!=0) {
+                    mViewPager.setCurrentItem(0);
+                    updateButton(false, true);
+                }
+            }
+        });
+
+        mLastButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                if(mViewPager.getCurrentItem()!=mCrimes.size()-1) {
+                    mViewPager.setCurrentItem(mCrimes.size()-1);
+                    updateButton(true, false);
+                }
+            }
+        });
+    }
+
+
+
+    public void updateButton(boolean firstButton, boolean secondButton){
+        mFirstButton.setEnabled(firstButton);
+        mLastButton.setEnabled(secondButton);
     }
 
     public static Intent newIntent(Context packageContext, UUID crimeId){
